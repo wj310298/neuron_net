32bc37efb52d421df4047b63af36a08b8bea8403
wj310298 <wj310298@sina.com>
2015-09-02 16:05:20 +0800

    Update BACKPROP.hpp

--- NNet/BACKPROP.hpp
+++ NNet/BACKPROP.hpp
@@ -1,5 +1,8 @@
 #ifndef _BACKPROP_HPP
 #define _BACKPROP_HPP
+
+#include <stdio.h>
+
 #define MAXLAYERS 4
 #define MAXNEURONS 120
 #define MAXPATTERNS 80
@@ -19,5 +22,56 @@ private:
 	double W[MAXLAYERS][MAXNEURONS][MAXNEURONS];		//WEIGHTS MATRIX
 	double Wprev[MAXLAYERS][MAXNEURONS][MAXNEURONS];	//previous WEIGHTS for momentum
 	double Neuron[MAXLAYERS][MAXNEURONS];
+	double DELTAj[MAXLAYERS][MAXNEURONS];
+	double DELTA_Wij[MAXLAYERS][MAXNEURONS][MAXNEURONS];
+	double ERROR[MAXNEURONS];
+	double WorstErr;
+	double AvgErr;
+	double LastAvgErr;
+	double LastWorstErr;
+	//Topology
+	int NumLayers;				//Number of layers
+	int OutLayerIndx;			//array index of last layer
+	int LayerSize[MAXLAYERS];	//Number of neurons in each layer
+	//Pattern data
+	double InPattern[MAXPATTERNS][MAXNEURONS];	//Input values for each pattern
+	double Desired[MAXPATTERNS][MAXNEURONS];	//Desired value for each pattern/output
+	unsigned int PatPresOrder[MAXPATTERNS];		//Order to present patterns to net
+	unsigned long int CurrIter;					//Current iterations number
+	long int Epoch;
+	int NumPatterns;							//Total patterns in training set
+	int CurrPat;								//Current pattern used in training
+	int ConvCount;								//The number of consecutive patterns within tolerance
+	int ConvergeFlg;							//Flag indicates convergence has occurred
+	//PARMS
+	double Temperature;							//For sigmoid
+	double ETA;									//Learning rate
+	double ALPHA;								//Momentum
+	double ERRTOL;								//Min error tolerance required for convergence
+	unsigned long int MAXITER;					//Max iterations to do before stopping
+
+public:
+	BackProp(void);
+	void LoadTrainingSet(char *Fname);
+	void GetInputs(void);
+	void RunNet(void);
+	void GetParms(char *);
+	void GetWeights(void);
+	double Sigmoid(double Net, double Tempr);
+	void SetRandomWeights(void);
+	double HCalcDelta(int lyr, int j, double dNETj);
+	int CalcErrors(void);
+	void AdaptWeights(void);
+	void SaveWeights(char *);
+	int train(char *, char *);
+	inline long int QueryEpoch() { return Epoch; }
+	inline double QueryTemparature() { return Temperature; }
+	inline double QueryEta() { return ETA; }
+	inline double QueryAlpha() { return ALPHA; }
 };
+
+//Misc flags
+int ArchOn = AVERAGE;	//Set to 1=ALL, NONE, AVERAGE or WORST to control data sent to ARCHIVE file for graphical analysis
+
+FILE *ARCHIVE;			//Archive training sequence
 #endif
